0e35b022a3a769b781c06fb12b1103617a1faf95
TinderSmith <coflery@gmail.com>
2020-05-25 14:30:53 +0800

    add domain to ip convert

--- nb.go
+++ nb.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 	"sync"
@@ -19,12 +18,12 @@ func main() {
 	//log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
 	log.SetFlags(log.Ldate | log.Lmicroseconds)
 
-	printWelcome()
+	printProjectInfo()
 
 	args := os.Args
 	argc := len(os.Args)
 	if argc <= 2 {
-		printHelp()
+		printUsage()
 		os.Exit(0)
 	}
 
@@ -38,114 +37,104 @@ func main() {
 		logPath = strings.Replace(logPath, "//", "/", -1)
 		logFile, logFileError = os.OpenFile(logPath, os.O_APPEND|os.O_CREATE, 0666)
 		if logFileError != nil {
-			log.Fatalln("[x]", "log file path error.", logFileError.Error())
+			log.Fatalln("[x ]", "日志文件路径错误.", logFileError.Error())
 		}
-		log.Println("[√]", "open test log file success. path:", logPath)
+		log.Println("[√]", "打开测试日志文件成功. 路径:", logPath)
 	}*/
 
 	switch args[1] {
 	case "-listen":
 		if argc < 3 {
-			log.Fatalln(`-listen need two arguments, like "nb -listen 1997 2017".`)
+			log.Fatalln(`-listen 需要两个参数, 例如 "nb -listen 2019 2020".`)
 		}
 		port1 := checkPort(args[2])
 		port2 := checkPort(args[3])
-		log.Println("[√]", "start to listen port:", port1, "and port:", port2)
+		log.Println("[√]", "开始监听端口:", port1, "与端口:", port2)
 		port2port(port1, port2)
 		break
 	case "-tran":
 		if argc < 3 {
-			log.Fatalln(`-tran need two arguments, like "nb -tran 1997 192.168.1.2:3389".`)
+			log.Fatalln(`-tran 需要两个参数, 例如 "nb -tran 2019 192.168.1.2:3389".`)
 		}
-		port := checkPort(args[2])
-		var remoteAddress string
-		if checkIp(args[3]) {
-			remoteAddress = args[3]
+		localPort := checkPort(args[2])
+
+		conn, err := net.ResolveTCPAddr("tcp", args[3])
+		if err != nil {
+			log.Fatalln("[x ]", "目标地址错误. 地址应该像这样[ip:port] 或者 [domain:port]. ")
 		}
-		split := strings.SplitN(remoteAddress, ":", 2)
-		log.Println("[√]", "start to transmit address:", remoteAddress, "to address:", split[0]+":"+port)
-		port2host(port, remoteAddress)
+		remoteIP := conn.IP.String()
+		remotePort := strconv.Itoa(conn.Port)
+		remoteIPAndPort := remoteIP + ":" + remotePort
+
+		log.Println("[√]", "开始传输端口:", localPort, "到地址:", remoteIPAndPort)
+		port2host(localPort, remoteIPAndPort)
 		break
 	case "-slave":
 		if argc < 3 {
-			log.Fatalln(`-slave need two arguments, like "nb -slave 127.0.0.1:3389 8.8.8.8:1997".`)
+			log.Fatalln(`-slave 需要两个参数, 例如 "nb -slave 127.0.0.1:3389 8.8.8.8:2019".`)
 		}
 		var address1, address2 string
-		checkIp(args[2])
-		if checkIp(args[2]) {
-			address1 = args[2]
+
+		conn, err := net.ResolveTCPAddr("tcp", args[2])
+		if err != nil {
+			log.Fatalln("[x ]", "地址错误. 地址应该像这样[ip:port] 或者 [domain:port]. ")
 		}
-		checkIp(args[3])
-		if checkIp(args[3]) {
-			address2 = args[3]
+		address1 = conn.IP.String() + ":" + strconv.Itoa(conn.Port)
+
+		conn, err = net.ResolveTCPAddr("tcp", args[3])
+		if err != nil {
+			log.Fatalln("[x ]", "地址错误. 地址应该像这样[ip:port] 或者 [domain:port]. ")
 		}
-		log.Println("[√]", "start to connect address:", address1, "and address:", address2)
+		address2 = conn.IP.String() + ":" + strconv.Itoa(conn.Port)
+
+		log.Println("[√]", "开始连接地址:", address1, "与地址:", address2)
 		host2host(address1, address2)
 		break
 	default:
-		printHelp()
+		printUsage()
 	}
 }
 
-func printWelcome() {
+func printProjectInfo() {
 	fmt.Println("+----------------------------------------------------------------+")
-	fmt.Println("| Welcome to use NATBypass Ver1.0.0 .                            |")
-	fmt.Println("| Code by cw1997 at 2017-10-19 03:59:51                          |")
-	fmt.Println("| If you have some problem when you use the tool,                |")
-	fmt.Println("| please submit issue at : https://github.com/cw1997/NATBypass . |")
+	fmt.Println("| NATBypass V2.0                                                 |")
+	fmt.Println("| Submit issue at : https://github.com/coflery/NATBypass         |")
 	fmt.Println("+----------------------------------------------------------------+")
 	fmt.Println()
-	// sleep one second because the fmt is not thread-safety.
-	// if not to do this, fmt.Print will print after the log.Print.
+	// 睡1秒,因为fmt不是线程安全的.如果不这样做,fmt.Print会在log.Print之后采能打印出
 	time.Sleep(time.Second)
 }
-func printHelp() {
-	fmt.Println(`usage: "-listen port1 port2" example: "nb -listen 1997 2017" `)
-	fmt.Println(`       "-tran port1 ip:port2" example: "nb -tran 1997 192.168.1.2:3389" `)
-	fmt.Println(`       "-slave ip1:port1 ip2:port2" example: "nb -slave 127.0.0.1:3389 8.8.8.8:1997" `)
+func printUsage() {
+	fmt.Println(`用法: "-listen port1 port2" 例: "nb -listen 2019 2020" `)
+	fmt.Println(`      "-tran port1 ip:port2" 例: "nb -tran 2019 192.168.1.2:3389" `)
+	fmt.Println(`      "-slave ip1:port1 ip2:port2" 例: "nb -slave 127.0.0.1:3389 8.8.8.8:2019" `)
 	fmt.Println(`============================================================`)
-	fmt.Println(`optional argument: "-log logpath" . example: "nb -listen 1997 2017 -log d:/nb" `)
-	fmt.Println(`log filename format: Y_m_d_H_i_s-agrs1-args2-args3.log`)
+	fmt.Println(`可选参数: "-log logpath" . 例: "nb -listen 2019 2020 -log d:/nb" `)
+	fmt.Println(`日志文件名格式: Y_m_d_H_i_s-agrs1-args2-args3.log`)
 	fmt.Println(`============================================================`)
-	fmt.Println(`if you want more help, please read "README.md". `)
+	fmt.Println(`如果你需要更多帮助,请阅读"README.md". `)
 }
 
 func checkPort(port string) string {
 	PortNum, err := strconv.Atoi(port)
 	if err != nil {
-		log.Fatalln("[x]", "port should be a number")
+		log.Fatalln("[x ]", "端口号应该是数字")
 	}
 	if PortNum < 1 || PortNum > 65535 {
-		log.Fatalln("[x]", "port should be a number and the range is [1,65536)")
+		log.Fatalln("[x ]", "端口号应该在1到65535之间")
 	}
 	return port
 }
 
-func checkIp(address string) bool {
-	ipAndPort := strings.Split(address, ":")
-	if len(ipAndPort) != 2 {
-		log.Fatalln("[x]", "address error. should be a string like [ip:port]. ")
-	}
-	ip := ipAndPort[0]
-	port := ipAndPort[1]
-	checkPort(port)
-	pattern := `^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$`
-	ok, err := regexp.MatchString(pattern, ip)
-	if err != nil || !ok {
-		log.Fatalln("[x]", "ip error. ")
-	}
-	return ok
-}
-
 func port2port(port1 string, port2 string) {
-	listen1 := start_server("0.0.0.0:" + port1)
-	listen2 := start_server("0.0.0.0:" + port2)
-	log.Println("[√]", "listen port:", port1, "and", port2, "success. waiting for client...")
+	listen1 := startServer("0.0.0.0:" + port1)
+	listen2 := startServer("0.0.0.0:" + port2)
+	log.Println("[√]", "监听端口:", port1, "与", port2, "成功. 等待连接...")
 	for {
 		conn1 := accept(listen1)
 		conn2 := accept(listen2)
 		if conn1 == nil || conn2 == nil {
-			log.Println("[x]", "accept client faild. retry in ", timeout, " seconds. ")
+			log.Println("[x ]", "接受连接失败. 在 ", timeout, " 秒后重试. ")
 			time.Sleep(timeout * time.Second)
 			continue
 		}
@@ -154,7 +143,7 @@ func port2port(port1 string, port2 string) {
 }
 
 func port2host(allowPort string, targetAddress string) {
-	server := start_server("0.0.0.0:" + allowPort)
+	server := startServer("0.0.0.0:" + allowPort)
 	for {
 		conn := accept(server)
 		if conn == nil {
@@ -162,17 +151,17 @@ func port2host(allowPort string, targetAddress string) {
 		}
 		//println(targetAddress)
 		go func(targetAddress string) {
-			log.Println("[+]", "start connect host:["+targetAddress+"]")
+			log.Println("[+]", "开始连接主机:["+targetAddress+"]")
 			target, err := net.Dial("tcp", targetAddress)
 			if err != nil {
 				// temporarily unavailable, don't use fatal.
-				log.Println("[x]", "connect target address ["+targetAddress+"] faild. retry in ", timeout, "seconds. ")
+				log.Println("[x ]", "连接目标地址 ["+targetAddress+"] 失败. 在 ", timeout, "秒后重试. ")
 				conn.Close()
-				log.Println("[←]", "close the connect at local:["+conn.LocalAddr().String()+"] and remote:["+conn.RemoteAddr().String()+"]")
+				log.Println("[←]", "关闭本地连接:["+conn.LocalAddr().String()+"] 和远程:["+conn.RemoteAddr().String()+"]")
 				time.Sleep(timeout * time.Second)
 				return
 			}
-			log.Println("[→]", "connect target address ["+targetAddress+"] success.")
+			log.Println("[→]", "连接目标地址 ["+targetAddress+"] 成功.")
 			forward(target, conn)
 		}(targetAddress)
 	}
@@ -180,26 +169,26 @@ func port2host(allowPort string, targetAddress string) {
 
 func host2host(address1, address2 string) {
 	for {
-		log.Println("[+]", "try to connect host:["+address1+"] and ["+address2+"]")
+		log.Println("[+]", "尝试连接主机:["+address1+"] 与 ["+address2+"]")
 		var host1, host2 net.Conn
 		var err error
 		for {
 			host1, err = net.Dial("tcp", address1)
 			if err == nil {
-				log.Println("[→]", "connect ["+address1+"] success.")
+				log.Println("[→]", "连接 ["+address1+"] 成功.")
 				break
 			} else {
-				log.Println("[x]", "connect target address ["+address1+"] faild. retry in ", timeout, " seconds. ")
+				log.Println("[x ]", "连接目标地址 ["+address1+"] 失败. 在 ", timeout, " 秒后重试. ")
 				time.Sleep(timeout * time.Second)
 			}
 		}
 		for {
 			host2, err = net.Dial("tcp", address2)
 			if err == nil {
-				log.Println("[→]", "connect ["+address2+"] success.")
+				log.Println("[→]", "连接 ["+address2+"] 成功.")
 				break
 			} else {
-				log.Println("[x]", "connect target address ["+address2+"] faild. retry in ", timeout, " seconds. ")
+				log.Println("[x ]", "连接目标地址 ["+address2+"] 失败. 在 ", timeout, " 秒后重试. ")
 				time.Sleep(timeout * time.Second)
 			}
 		}
@@ -207,22 +196,22 @@ func host2host(address1, address2 string) {
 	}
 }
 
-func start_server(address string) net.Listener {
-	log.Println("[+]", "try to start server on:["+address+"]")
+func startServer(address string) net.Listener {
+	log.Println("[+]", "尝试开始服务于端口:["+address+"]")
 	server, err := net.Listen("tcp", address)
 	if err != nil {
-		log.Fatalln("[x]", "listen address ["+address+"] faild.")
+		log.Fatalln("[x ]", "监听地址 ["+address+"] 失败.")
 	}
-	log.Println("[√]", "start listen at address:["+address+"]")
+	log.Println("[√]", "开始监听地址:["+address+"]")
 	return server
 	/*defer server.Close()
 
 	for {
 		conn, err := server.Accept()
-		log.Println("accept a new client. remote address:[" + conn.RemoteAddr().String() +
-			"], local address:[" + conn.LocalAddr().String() + "]")
+		log.Println("接受一个新连接失败. 远端地址:[" + conn.RemoteAddr().String() +
+			"], 本地地址:[" + conn.LocalAddr().String() + "]")
 		if err != nil {
-			log.Println("accept a new client faild.", err.Error())
+			log.Println("接受一个新连接失败.", err.Error())
 			continue
 		}
 		//go recvConnMsg(conn)
@@ -232,15 +221,15 @@ func start_server(address string) net.Listener {
 func accept(listener net.Listener) net.Conn {
 	conn, err := listener.Accept()
 	if err != nil {
-		log.Println("[x]", "accept connect ["+conn.RemoteAddr().String()+"] faild.", err.Error())
+		log.Println("[x ]", "接受连接 ["+conn.RemoteAddr().String()+"] 失败.", err.Error())
 		return nil
 	}
-	log.Println("[√]", "accept a new client. remote address:["+conn.RemoteAddr().String()+"], local address:["+conn.LocalAddr().String()+"]")
+	log.Println("[√]", "收到一个新连接. 远端地址:["+conn.RemoteAddr().String()+"], 本地地址:["+conn.LocalAddr().String()+"]")
 	return conn
 }
 
 func forward(conn1 net.Conn, conn2 net.Conn) {
-	log.Printf("[+] start transmit. [%s],[%s] <-> [%s],[%s] \n", conn1.LocalAddr().String(), conn1.RemoteAddr().String(), conn2.LocalAddr().String(), conn2.RemoteAddr().String())
+	log.Printf("[+] 开始传输. [%s],[%s] <-> [%s],[%s] \n", conn1.LocalAddr().String(), conn1.RemoteAddr().String(), conn2.LocalAddr().String(), conn2.RemoteAddr().String())
 	var wg sync.WaitGroup
 	// wait tow goroutines
 	wg.Add(2)
@@ -260,9 +249,9 @@ func connCopy(conn1 net.Conn, conn2 net.Conn, wg *sync.WaitGroup) {
 		io.Copy(conn1, conn2)
 	}
 	conn1.Close()
-	log.Println("[←]", "close the connect at local:["+conn1.LocalAddr().String()+"] and remote:["+conn1.RemoteAddr().String()+"]")
+	log.Println("[←]", "关闭本地连接:["+conn1.LocalAddr().String()+"] 和远程:["+conn1.RemoteAddr().String()+"]")
 	//conn2.Close()
-	//log.Println("[←]", "close the connect at local:["+conn2.LocalAddr().String()+"] and remote:["+conn2.RemoteAddr().String()+"]")
+	//log.Println("[←]", "关闭本地连接:["+conn2.LocalAddr().String()+"] 和远程:["+conn2.RemoteAddr().String()+"]")
 	wg.Done()
 }
 func openLog(address1, address2, address3, address4 string) *os.File {
@@ -275,15 +264,15 @@ func openLog(address1, address2, address3, address4 string) *os.File {
 		address2 = strings.Replace(address2, ":", "_", -1)
 		address3 = strings.Replace(address3, ":", "_", -1)
 		address4 = strings.Replace(address4, ":", "_", -1)
-		timeStr := time.Now().Format("2006_01_02_15_04_05") // "2006-01-02 15:04:05"
+		timeStr := time.Now().Format("yyyy_mm_dd_HH_mm_ss") // "2000-12-31 23:59:59"
 		logPath := args[5] + "/" + timeStr + args[1] + "-" + address1 + "_" + address2 + "-" + address3 + "_" + address4 + ".log"
 		logPath = strings.Replace(logPath, `\`, "/", -1)
 		logPath = strings.Replace(logPath, "//", "/", -1)
 		logFile, logFileError = os.OpenFile(logPath, os.O_APPEND|os.O_CREATE, 0666)
 		if logFileError != nil {
-			log.Fatalln("[x]", "log file path error.", logFileError.Error())
+			log.Fatalln("[x ]", "日志文件路径错误.", logFileError.Error())
 		}
-		log.Println("[√]", "open test log file success. path:", logPath)
+		log.Println("[√]", "打开测试日志文件成功. 路径:", logPath)
 	}
 	return logFile
 }
